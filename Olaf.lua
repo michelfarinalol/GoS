361d0dbf9e0573ddf7bd9f2b03f90a8af98afc42
wildrelic <ttelesca1@hotmail.com>
2016-07-03 16:37:15 -0400

    Olaf.lua
    First Script. Has some problems. Possible lag, trouble targeting and attacking jungle creeps like dragon, I want to add circles for the Axe location.

---
+++ Olaf.lua
@@ -0,0 +1,113 @@
+if GetObjectName(GetMyHero()) ~= "Olaf" then return end
+
+require("OpenPredict")
+
+--Menu--
+OlafMenu = Menu("Olaf", "Olaf")
+-----------------------------------------------------------------
+OlafMenu:SubMenu("Combo", "Combo")
+OlafMenu.Combo:Boolean("Q", "Use Q", true)
+OlafMenu.Combo:Boolean("W", "Use W", true)
+OlafMenu.Combo:Slider("uW", "Use W at % HP", 75, 5, 100, 5)
+OlafMenu.Combo:Boolean("E", "Use E", true)
+-----------------------------------------------------------------
+OlafMenu:SubMenu("JC", "JungleClear / LaneClear")
+OlafMenu.JC:Boolean("Q", "Use Q", true)
+OlafMenu.JC:Boolean("W", "Use W", true)
+OlafMenu.JC:Boolean("E", "Use E", true)
+-----------------------------------------------------------------
+OlafMenu:SubMenu("KS", "Killsteal")
+OlafMenu.KS:Boolean("Q", "Use Q", true)
+OlafMenu.KS:Boolean("E", "Use E", true)
+-----------------------------------------------------------------
+OlafMenu:SubMenu("Draw", "Drawings")
+OlafMenu.Draw:Boolean("DrawQ", "Draw Q Range", true)
+OlafMenu.Draw:Boolean("DrawE", "Draw E Range", true)
+-----------------------------------------------------------------
+
+
+
+local OlafQ = {delay = 0.25, speed = 1550, width = 100, range = 1000}
+
+OnTick(function()
+	local target = GetCurrentTarget()
+	
+--Combo--
+
+	if IOW:Mode() == "Combo" then
+		if OlafMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 325) then
+			CastTargetSpell(target, _E)
+			end
+		if OlafMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 250) then
+			CastSpell(_W)
+			end
+		if OlafMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 1000) then
+			local QPred = GetLinearAOEPrediction(target, OlafQ)
+			if QPred.hitChance >= 0.3 then
+				CastSkillShot(_Q, QPred.castPos)
+			end
+		end
+	end
+	
+--Killsteal--
+
+	for _,enemy in pairs(GetEnemyHeroes()) do
+		if OlafMenu.KS.Q:Value() and Ready(_Q) and ValidTarget(enemy, 1000) then
+			if GetCurrentHP(enemy) + GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, GetCastLevel(myHero, _Q) * 45 + 25 + GetBonusDmg(myHero) * 1) then
+				local QPred = GetLinearAOEPrediction(enemy, OlafQ)
+				if QPred.hitChance >= 0.3 then
+					CastSkillShot(_Q, QPred.castPos)
+				end
+			end
+		end
+		if OlafMenu.KS.E:Value() and Ready(_Q) and ValidTarget(enemy, 325) then
+			if GetCurrentHP(enemy) + GetDmgShield(enemy) < CalcDamage(myHero, enemy, 0, GetCastLevel(myHero, _E) * 45 + 25 + GetBaseDamage(myHero) * 0.4) then
+				CastTargetSpell(enemy, _E)
+			end
+		end
+	end
+	
+--JungleClear--
+
+	if IOW:Mode() == "LaneClear" then
+		for _, mob in pairs(minionManager.objects) do
+			if OlafMenu.JC.E:Value() and Ready(_E) and ValidTarget(mob, 325) then
+				CastTargetSpell(mob, _E)
+			end
+			if OlafMenu.JC.W:Value() and Ready(_W) and ValidTarget(mob, 225) then
+				CastSpell(_W)
+			end
+			if OlafMenu.JC.Q:Value() and Ready(_Q) and ValidTarget(mob, 1000) then
+				CastSkillShot(_Q, mob)
+			end
+		end
+	end
+end)
+
+--Drawings--
+
+OnDraw(function(myHero)
+
+	if IsObjectAlive(myHero) then
+	
+--Q Drawings--
+
+		if OlafMenu.Draw.DrawQ:Value() then
+			if Ready(_Q) then
+				DrawCircle(GetOrigin(myHero), 1000, 5, 100, ARGB(100, 0, 225, 0))
+			else
+				DrawCircle(GetOrigin(myHero), 1000, 5, 100, ARGB(100, 225, 0, 0))
+			end
+		end
+		
+--E Drawings--
+
+		if OlafMenu.Draw.DrawE:Value() then
+			if Ready(_E) then
+				DrawCircle(GetOrigin(myHero), 325, 5, 100, ARGB(100, 0, 225, 0))
+			else
+				DrawCircle(GetOrigin(myHero), 325, 5, 100, ARGB(100, 225, 0, 0))
+			end
+		end
+	end
+end)
