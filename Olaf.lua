7f0a81ffdca9dee48d4ac2fbdea652d614b909d8
wildrelic <ttelesca1@hotmail.com>
2016-07-07 16:33:21 -0400

    Update Olaf.lua

--- Olaf.lua
+++ Olaf.lua
@@ -1,6 +1,6 @@
 if GetObjectName(GetMyHero()) ~= "Olaf" then return end
 
-local v = 8
+local v = 9
 
 GetWebResultAsync("https://raw.githubusercontent.com/wildrelic/GoS/master/Olaf.version", function(num)
 	if v < tonumber(num) then
