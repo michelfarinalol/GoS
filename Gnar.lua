e90d076a3951e4be53730cce80c2f8fc07a80efb
wildrelic <ttelesca1@hotmail.com>
2016-07-09 17:24:52 -0400

    Update Gnar.lua

--- Gnar.lua
+++ Gnar.lua
@@ -1,1 +1,13 @@
+if GetObjectName(GetmyHero()) ~= "Gnar" then return end
 
+local v = 0.1
+
+GetWebResultAsync("https://raw.githubusercontent.com/wildrelic/GoS/master/Gnar.version", function(num)
+	if v < tonumber(num) then
+		PrintChat("[Gnar] Update Available. x2 f6 to Update.")
+		DownloadFileAsync("https://raw.githubusercontent.com/wildrelic/GoS/master/Gnar.lua", SCRIPT_PATH .. "Gnar.lua", function() PrintChat("Enjoy your game!") return end)
+	end
+end)
+
+require("OpenPredict")
+require("MapPositionGos")
