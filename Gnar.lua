5343e1a73a215e99aaad922669659c69c69870cd
wildrelic <ttelesca1@hotmail.com>
2016-07-11 09:33:32 -0400

    Update Gnar.lua

--- Gnar.lua
+++ Gnar.lua
@@ -1,6 +1,6 @@
 if GetObjectName(GetMyHero()) ~= "Gnar" then return end
 
-local v = 0.20
+local v = 0.21
 
 GetWebResultAsync("https://raw.githubusercontent.com/wildrelic/GoS/master/Gnar.version", function(num)
 	if v < tonumber(num) then
